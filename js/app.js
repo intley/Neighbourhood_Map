0ce905bcde57e4022ef261614601158b7234213a
Intley <intley@gmail.com>
2018-01-09 13:58:01 -0800

    InfoWindow Fix

--- js/app.js
+++ js/app.js
@@ -1,123 +1,187 @@
-// Creating the required global map variables
-var map, bounds, infowindow;
+// Global Map variable
+var map, bounds, infoWindow;
+
+// Creating a blank array for all markers
 var markers = [];
 
-/* Model for the app. Neighbourhood places that I frequent in my locality. */
+/* Model for the app
+   Famous Hotels in my locality
+*/
 
-var locations = [
-  {
-    "name": 'Popeyes Louisiana Kitchen',
-    "location": {lat: 40.745754, lng: -74.163797}
-  },
-  {
-    "name": '5 Grains Rice',
-    "location": {lat: 40.746177, lng: -74.158874}
-  },
-  {
-    "name": 'Gina\'s Pizzeria',
-    "location": {lat: 40.750263, lng: -74.156915}
-  },
-  {
-    "name": 'Quikchek',
-    "location": {lat: 40.743864, lng: -74.155746}
-  },
-  {
-    "name": 'Wendy\'s',
-    "location": {lat: 40.743100, lng: -74.155389}
-  },
-  {
-    "name": 'Tops Diner',
-    "location": {lat: 40.750716, lng: -74.164004}
-  }
-]
+var Location = function(response) {
+  this.name = response.name;
+  this.location = response.location;
+  this.address = response.address;
+  this.contact = response.contact;
 
+  this.marker = new google.maps.Marker({
+    map: map,
+    position: this.location,
+    title: this.name,
+    animation: google.maps.Animation.DROP
+  });
 
-// ViewModel for the application
-var ViewModel = function() {
   var self = this;
 
-  self.searchInput = ko.observable('');
-  self.locationList = ko.observableArray([]);
-
-  self.locationList.push(locations.name)
-
-  /*
-  locations.forEach(function(location) {
-    self.locationList.push(locations.location);
-  });*/
-
-self.filteredLocations = ko.computed(function() {
-  var filterText = self.searchInput().toLowerCase();
-
-  return ko.utils.arrayFilter(self.locationList(), function(location) {
-    if (locations.title.toLowerCase().indexOf(filterText) != -1) {
-      locations.marker.setMap(map);
-      return true;
-    } else {
-      locations.marker.setMap(null);
-      return false;
-    }
+  // Create an onclick event to open an infowindow at each marker.
+  this.marker.addListener('click', function() {
+  populateInfoWindow(self);
   });
-});
+
+  // Extend the boundaries of the map for each marker
+  bounds.extend(this.position);
+
+};
+
+
+/* ViewModel for the application,
+   Binds the model with the view and controls
+   the functioning of the application.
+*/
+
+var ViewModel = function() {
+  var self = this;
+  self.markerList = ko.observableArray([]);
+
+  //console.log(markerList);
+
+  markers.forEach(function(location) {
+    self.markerList.push(new Location(location));
+  });
+
+  self.searchInput = ko.observable('');
+
+  self.filteredLocations = ko.computed(function() {
+    var searchInput = self.searchInput().toLowerCase();
+
+    return ko.utils.arrayFilter(self.markerList(), function(location) {
+      if (location.name.toLowerCase().indexOf(searchInput) != -1) {
+        location.marker.setMap(map);
+        return true;
+      } else {
+        location.marker.setMap(null);
+        return false;
+      }
+    });
+  });
+
+  self.openInfoWindow = function(response) {
+    populateInfoWindow(response);
+  };
+
+};
+
+// Applying Bindings to the View
+function initApp() {
+  ko.applyBindings(new ViewModel());
 }
 
-ko.applyBindings(new ViewModel());
 
+/* FourSquare API call
+   Retrieves data and creates markers to populate map
+*/
 
+function getLocationData(center, categoryId, radius, limit) {
+  var api_call ='https://api.foursquare.com/v2/venues/search' +
+                '?ll=' + center.lat + ',' + center.lng +
+                '&categoryId=' + categoryId +
+                '&radius=' + radius +
+                '&limit=' + limit +
+                '&v=20180107&client_id=' +
+                'O3A0RUJ0BZSQWVORUVVP12CMMYH2FVIDSUA02BIQMAJFELWO' +
+                '&client_secret=' +
+                'JBBFQMM0PT3PLANGLPNY5UVDINAL3KIE0E5BEUNEFJFDVFB0';
+//console.log(api_call)
 
+$.ajax({
+  type: 'GET',
+  url: api_call,
+  dataType: 'json',
+  success: function(response) {
+    venues = response['response']['venues'];
+    venues.forEach(function (venue) {
+      var name = venue['name'];
+      var location = venue['location'];
+      var coord = {
+        lat: location.lat,
+        lng: location.lng
+      };
+      var address = location['formattedAddress'];
+      var contact = venue['contact'].formattedPhone;
 
-// Setting the Google Map object with the customized settings
+      markers.push({
+        name: name,
+        location: coord,
+        address: address,
+        contact: contact,
+      });
+    });
+
+    //console.log(markers);
+
+    // Initialize Knockout bindings
+    initApp();
+
+    // Extend the boundaries of the map for each marker
+    map.fitBounds(bounds);
+    }
+  });
+}
+
+// Setting the Google Map with the required settings
+
 function initMap() {
-var myLatlng = new google.maps.LatLng(40.7465, -74.1563);
-var largeInfowindow = new google.maps.InfoWindow();
-var bounds = new google.maps.LatLngBounds();
+var myLatlng = {
+  lat: 40.7413549,
+  lng: -73.9980244
+}
+//var myLatlng = new google.maps.LatLng(40.7413549, -73.9980244);
 
 var mapOptions = {
-  zoom: 14,
-  center: myLatlng,
-  mapTypeId: 'roadmap',
-
+  zoom: 13,
+  center: myLatlng
+//  mapTypeId: 'roadmap'
 };
 
 map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-// The following group uses the location array to create an array of markers on initialize.
-for (var i = 0; i < locations.length; i++) {
-// Get the position from the location array.
-var position = locations[i].location;
-var title = locations[i].name;
-// Create a marker per location, and put into markers array.
-var marker = new google.maps.Marker({
-map: map,
-position: position,
-title: title,
-animation: google.maps.Animation.DROP,
-id: i
-});
-// Push the marker to our array of markers.
-markers.push(marker);
-// Create an onclick event to open an infowindow at each marker.
-marker.addListener('click', function() {
-populateInfoWindow(this, largeInfowindow);
-});
-bounds.extend(markers[i].position);
-}
-// Extend the boundaries of the map for each marker
-map.fitBounds(bounds);
+infoWindow = new google.maps.InfoWindow();
+bounds = new google.maps.LatLngBounds();
+
+getLocationData(myLatlng, '4deefb944765f83613cdba6e', 4000, 30);
+
 }
 
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
-function populateInfoWindow(marker, infowindow) {
-// Check to make sure the infowindow is not already opened on this marker.
-if (infowindow.marker != marker) {
-infowindow.marker = marker;
-infowindow.setContent('<div>' + marker.title + '</div>');
-infowindow.open(map, marker);
-// Make sure the marker property is cleared if the infowindow is closed.
-infowindow.addListener('closeclick',function(){
-infowindow.setMarker = null;
-});
-}
+// This function populates the infoWindow when the marker is clicked
+function populateInfoWindow(location) {
+  var marker = location.marker;
+  // Check to make sure the infoWindow is not already opened on this marker
+  if (infoWindow.marker != marker) {
+    // Start BOUNCE animation
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+
+    infoWindow.marker = marker;
+    infoWindow.setContent(`
+      <div>
+        <h4>` + location.name + `</h4>
+        ` + (location.contact === undefined ? '' : '<div>' + location.contact + '</div>') + `
+        <br>
+        <h6>Address</h6>
+        <div>` + location.address[0] + `</div>
+        <div>` + location.address[1] + `</div>
+        <div>` + location.address[2] + `</div>
+      </div>
+    `);
+    infoWindow.open(map, marker);
+
+    // Make sure the marker property is cleared if the infoWindow is closed
+    infoWindow.addListener('closeclick', function() {
+      infoWindow.marker = null;
+    });
+
+    // Set timeout for animation
+    setTimeout(function() {
+      marker.setAnimation(null);
+    }, 700);
+  }
 }
